ce5cb7b23348365d9d47d65219c849bd6f280203
felipe estrella <felipee.ti23@utsjr.edu.mx>
2024-09-02 14:04:27 -0600

    first commit

---
+++ routes/rutas.js
@@ -0,0 +1,15 @@
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+const { fecha, hora, saludo } = require('../test/fecha-hora');
+
+router.use(saludo); 
+router.use(fecha);  
+router.use(hora);   
+
+router.get('/', (req, res) => {
+    console.log(`La fecha actual es ${req.fechaActual} y la hora actual es ${req.horaActual}`);
+    res.send('Consulta realizada, verifica la consola.');
+});
+
+module.exports = router;
